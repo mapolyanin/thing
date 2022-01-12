4bc939fcd6514d2b35dd98d788b3d8eafa42c668
mapolyanin <mi.al.pol@gmail.com>
2022-01-12 19:58:48 +0300

    add card class

--- thing_server/model.py
+++ thing_server/model.py
@@ -7,29 +7,30 @@ Created on Wed Jan 12 14:39:44 2022
 
 import secrets
 import random
+from typing import List
 
-import test_config
+
 
 class Game():
     def __init___(self, state):
         pass
     
-    def create_game():
+    def create_game(self):
         #Создаем игру
         pass
     
-    def start_game():
+    def start_game(self):
         #Начинаем игру
         pass
     
-    def get_card({token:''}):
+    def get_card(self):
         #получаем карты из колоды
         pass
     
-    def check_game_status():
+    def check_game_status(self):
         pass
     
-    def connect_to_game():
+    def connect_to_game(self):
         #возвращаем токен, номер игрока и имя игрока
         pass
     
@@ -40,7 +41,6 @@ class CardDeck():
         Всего карт в колоде
         Осталось карт в колоде
         Список карт в виде словаря:
-        
     Методы:
         Создание колоды карт в начале игры - create_deck
         Создать колоду из оставшихся карт
@@ -49,19 +49,54 @@ class CardDeck():
         Сбросить карту
         Создать колоду из сброшенных карт          
     """
-    def create_deck(self, number_of_player):
-        pass
-    
-    
+    def __init__(self, count_of_player:int)-> None:
+        self.start_deck = []
+        self.game_deck = []
+        self.cards_in_dec = 0
+        self.folded_cards = []
 
-    
+        
+    def create_deck(self, number_of_player:int):
+        """Какие карты попадают в колоду, зависит от количества игроков"""
+        pass
+
+    def get_card(self) -> int:
+        """Получить карту во время игры из колоды"""
+        pass
+
+    def fold_card(self, card: Card):
+        """Сброс карты в сброшенную колоду"""
+        pass
+
+    def shuffle_cards(self, deck:List) -> List:
+        """Перетасовать карты. Надо, чтобы он перетасовывал либо start_dec, либо game_dec
+        Ну или сделать два разных метода"""
+        pass
 
 
 class Card():
-    """ А оно надо? Пропишу все это как словарь в CardDeck"""
+    """
+    Свойства:
+    Тип карты:  событие или паника
+    Подтип карты: инфекция, действия, защиты, препятсвия
+    Действие карты: прописать через json
+    Применимость карты: к соседу или к любому
+    Карта закреплена: True False (для зараженных)
+    Применимо для количества игроков: int
+    """
+    def __init__(self, card_type:str, card_subtype: str, 
+    card_fix:bool, apply_to:str, max_players:int ) -> None:
+        self.card_type =  card_type # event, panic
+        self.card_subtype = card_subtype #infection, action, defense, barrier
+        self.card_fix = card_fix #True - если владелец заражен, False, если нет
+        self.apply_to = apply_to # neighbour, all
+        self.max_players = max_players
+
+
     pass
 
 class Player():
+    """Свойства """
     pass
 
 
